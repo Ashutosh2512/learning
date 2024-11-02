d173107ec746386a478a4b472200524d54351088
Ashutosh2512 <ashutoshbhardwaj25122000@gmail.com>
2024-11-02 20:35:40 +0530

    Adding first commit to test Azure + SpringBoot

---
+++ learning/src/main/java/com/test_azure/learning/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.test_azure.learning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Congratulations on launching the app!!";
+    }
+}
